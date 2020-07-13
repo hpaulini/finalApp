98aa801db2579dbc9dd26d7ba27a7a8c4835c534
Helen Paulini <hpaulini@fb.com>
2020-07-13 10:39:55 -0400

    Initial commit and parse configuration

---
+++ app/src/main/java/com/helenpaulini/ribbon_resources/MainActivity.java
@@ -0,0 +1,14 @@
+package com.helenpaulini.ribbon_resources;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/helenpaulini/ribbon_resources/ParseApplication.java
@@ -0,0 +1,21 @@
+package com.helenpaulini.ribbon_resources;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // set applicationId, and server server based on the values in the Heroku settings.
+        // clientKey is not needed unless explicitly configured
+        // any network interceptors must be added with the Configuration Builder given this syntax
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("ribbon-resources") // should correspond to APP_ID env variable
+                .clientKey("helenpauliniRibbonResources")  // set explicitly unless clientKey is explicitly configured on Parse server
+                .server("https://ribbon-resources.herokuapp.com/parse/").build());
+    }
+}
