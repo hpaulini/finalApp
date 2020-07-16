2e05628b3a5d5f4fde64ddd7552defbc8d1c859d
Helen Paulini <hpaulini@fb.com>
2020-07-16 17:47:03 -0400

    added test class

---
+++ app/src/androidTest/java/com/helenpaulini/ribbon_resources/utilities/MatchingTest.java
@@ -0,0 +1,23 @@
+package com.helenpaulini.ribbon_resources.utilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MatchingTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void match() {
+        assertEquals(0, 0);
+    }
+}
---
+++ app/src/main/java/com/helenpaulini/ribbon_resources/utilities/Matching.java
@@ -0,0 +1,12 @@
+package com.helenpaulini.ribbon_resources.utilities;
+
+import com.helenpaulini.ribbon_resources.models.PatientProfile;
+import com.helenpaulini.ribbon_resources.models.SurvivorProfile;
+
+public class Matching {
+
+    public static Integer match(PatientProfile patientProfile, SurvivorProfile survivorProfile){
+        return 0;
+    }
+
+}
